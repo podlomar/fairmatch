971edfade8020c63619b5c10aef6fc166c65cccd
Martin Podloucký <podlouckymartin@gmail.com>
2023-05-03 11:44:56 +0200

    Setup a fresh new Next.js project

---
+++ pages/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+// import css from './styles.module.scss';
+
+const HomePage = (): JSX.Element => {
+  return (
+    <h1>Fairmatch</h1> 
+  )
+};
+
+export default HomePage;
